e7c2d6dd8d7c81b529ae569c6d948a320e16b6c1
21081010027DiazPrisheila <21081010027@student.upnjatim.ac.id>
2023-05-12 12:39:56 +0700

    h

--- add.php
+++ add.php
@@ -9,7 +9,7 @@
         <img src="login.png" height="100" width="100" style="display:block; margin:auto;"/>
     </p>
     <form action="add.php" method="post" name="form1">
-        <table style="margin-left: auto; margin-right: auto; width=25%; border=0;">
+        <table style="margin:auto; width=25%; border=0;">
             <tr> <td>USERNAME</td> </tr>
             <tr> <td><input type="text" name="username"></td> </tr>
             
@@ -17,6 +17,9 @@
             <tr> <td><input type="text" name="password"></td> </tr>
 
             <tr><td><input type="submit" name="Submit" value="ENTER"></td> </tr>
+
+            <tr> <td>Belum Punya Akun?</td> </tr>
+            <tr> <td> <a href="signup.php"> SIGN UP </a> </td> </tr>
         </table>
     </form>
 
@@ -24,15 +27,14 @@
     <?php
 
     if(isset($_POST['Submit'])) {
-        $id_jenis = $_POST['id_jenis'];
-        $jenis = $_POST['jenis'];
-        $keterangan = $_POST['keterangan'];
+        $username = $_POST['username'];
+        $password = $_POST['password'];
 
         include("koneksi.php");
 
-        $hasil = mysql_query("INSERT INTO tb_jenis(id_jenis,jenis,keterangan) VALUES('$id_jenis','$jenis','$keterangan')");
+        $hasil = mysql_query("INSERT INTO employees(username, password) VALUES('$username','$password')");
 
-        echo "User added successfully. <a href='index.php'>View Users</a>";
+        echo "User added successfully. <a href='admin.php'>View Users</a>";
     }
     ?>
 </body>
